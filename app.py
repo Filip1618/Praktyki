0c597c2b0d766d50891ab1b262060a1e5a9e3f94
Filip <filipmikusek88@gmail.com>
2022-10-06 11:06:00 +0200

    Dodanie funkcji logowania oraz wylogowywania uzytkownika z sesji

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from wtforms import (StringField, SubmitField, PasswordField)
 from flask_wtf import FlaskForm
 from wtforms.validators import input_required
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user
 from dotenv import load_dotenv
 from os import getenv
 from flask_bcrypt import Bcrypt
@@ -19,18 +20,23 @@ APP_KEY=getenv('APP_KEY')
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}/{DATABASE_NAME}'
 app.config['SECRET_KEY'] = APP_KEY
 
 
-class Users(db.Model):
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class Users(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String(32), nullable=False, unique=True)
 	password = db.Column(db.String(64), nullable=False, unique=False)
 	firstname = db.Column(db.String(32),nullable=False, unique=False)
 	lastname = db.Column(db.String(32), nullable=False, unique=False)
-
 
 class Register_forms(FlaskForm):
     register_submit = SubmitField()
@@ -70,7 +76,6 @@ class Register_forms(FlaskForm):
         if len(username) > username_max:
             raise Exception("Nazwa użytkownika jest zbyt długa")
 
-
         if_exists = Users.query.filter_by(username = username).first()
         if if_exists:
             raise Exception("Taki użytkownik już istnieje")
@@ -106,7 +111,6 @@ def register():
     if register.validate_on_submit():
         if register.validation():
             hashed_password = bcrypt.generate_password_hash(register.register_password.data)
-            print(hashed_password)
             db.session.add(Users(username=register.register_username.data,
                             password=hashed_password,
                             lastname=register.register_lastname.data,
@@ -115,6 +119,34 @@ def register():
             return redirect(url_for('home'))
 
     return render_template('register.html', register=register)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login = Login_forms()
+    if request.method == "POST":
+        username = login.login_username.data
+        password = login.login_password.data
+        user = Users.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, login.login_password.data):
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            raise Exception("Hasło lub nazwa użytkownika jest błędna")
+
+    return render_template('login.html', login=login)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 
 if __name__ == '__main__':
