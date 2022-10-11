e9869ccbf58b941818b955d5b84d3866abfec4c1
Dawid <dawid.15@vp.pl>
2022-10-11 13:11:18 +0200

    Dodanie funkcjonalnego paska wyszukiwania filmów i seriali

--- static/home/app.js
+++ static/home/app.js
@@ -20,21 +20,21 @@ jQuery.extend({
 
 
 
-const getArrayOfTrendingMovies = () => {
+const getResults = (url) => {
 
-const json = $.getJSON('https://api.themoviedb.org/3/trending/all/day?api_key=87bf596c371c406133fdf1b253db9a36&language=pl')
-const results = json.results
+  const json = $.getJSON(url)
+  const results = json.results
 
-results.forEach(movie => {
+  results.forEach(movie => {
 
-  if (movie.title == undefined){
-    movie.title = movie.name
-    delete movie.name
-  }
+    if (movie.title == undefined){
+      movie.title = movie.name
+      delete movie.name
+    }
 
-})
+  })
 
-return results
+  return results
 }
 
 
@@ -81,13 +81,58 @@ function RenderRandomTrendMovie(props) {
   
 }
 
+function RenderSearchResults(props) {
+
+  const arrayOfMovies = props.arrayOfMovies
+  const listOfSearchResults = arrayOfMovies.slice(0, 3).map((movie) => 
+    <li key={movie.id}>
+      <a href={movie.id}>
+        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title}/>
+        <div className='container--info'>
+          <h3>{movie.title}</h3>
+          <p>{movie.overview == "" ? "Brak opisu :(" : `${movie.overview.substring(0,240)}...`}</p>
+        </div>
+      </a>
+    </li>
+  )
 
 
-const arrayOfMovies = getArrayOfTrendingMovies()
+  return(
+    <ul className="list--of--results">
+      {listOfSearchResults}
+      <a>Pokaż wiecej wyników</a>
+    </ul>
+  )
 
+}
+
+
+
+const arrayOfMovies = getResults('https://api.themoviedb.org/3/trending/all/day?api_key=87bf596c371c406133fdf1b253db9a36&language=pl')
 
 const randomMovie = ReactDOM.createRoot(document.getElementById("randomMovie"))
 randomMovie.render(<RenderRandomTrendMovie arrayOfMovies={arrayOfMovies}/>)
 
 const trendingMovies = ReactDOM.createRoot(document.getElementById('trendingMovies'))
 trendingMovies.render(<RenderListOfTrends arrayOfMovies={arrayOfMovies}/>)
+
+
+const searchArea = ReactDOM.createRoot(document.getElementById("searchArea"))
+document.getElementById('searchBar').addEventListener('input', () => {
+
+  var url = (`https://api.themoviedb.org/3/search/movie?api_key=87bf596c371c406133fdf1b253db9a36&language=pl&page=1&include_adult=false&query=${document.getElementById('searchBar').value}`)
+  const listOfResults = getResults(url)
+
+  searchArea.render(<RenderSearchResults arrayOfMovies={listOfResults}/>)
+
+})
+
+document.getElementById('mainContainer').addEventListener('click', () => {
+  document.getElementById('searchArea').style.display = 'none'
+})
+
+document.getElementById('searchBar').addEventListener('click', () => {
+  document.getElementById('searchArea').style.display = 'initial'
+})
+
+
