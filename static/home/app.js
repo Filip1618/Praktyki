ee5378d522df5f1255132c8351ea1b55a3377f17
Filip <filipmikusek88@gmail.com>
2022-10-12 02:21:13 +0200

    Poprawienie paska wyszukiwania (wyszukiwanie nie tylko filmów ale też seriali, wyświetlanie najpopularniejszych pozycji w pierwszej kolejności oraz zmienienie działania samego wyszukiwania, od teraz JS wysyła zapytania do API po 1 sekundzie braku aktywności w pasku, dzięki temu zapytania są wysyłane tylko wtedy gdy jest to konieczne.)

--- static/home/app.js
+++ static/home/app.js
@@ -1,138 +1,154 @@
 jQuery.extend({
+  getJSON: function (url) {
+    var result = null;
 
-  getJSON: function(url) {
-      var result = null;
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json",
+      async: false,
+      success: function (data) {
+        result = data;
+      },
+    });
 
-      $.ajax({
-          url: url,
-          type: 'get',
-          dataType: 'json',
-          async: false,
-          success: function(data) {
-              result = data;
-          }
-      });
-      
-     return result;
-  }
-
+    return result;
+  },
 });
 
 
-
 const getResults = (url) => {
+  const json = $.getJSON(url);
+  const results = json.results;
 
-  const json = $.getJSON(url)
-  const results = json.results
-
-  results.forEach(movie => {
-
-    if (movie.title == undefined){
-      movie.title = movie.name
-      delete movie.name
+  results.forEach((movie) => {
+    if (movie.title == undefined) {
+      movie.title = movie.name;
+      delete movie.name;
     }
+  });
 
-  })
-
-  return results
-}
-
+  return results;
+};
 
 
 function RenderListOfTrends(props) {
+  const arrayOfMovies = props.arrayOfMovies;
 
-const arrayOfMovies = props.arrayOfMovies;
+  const renderedArray = arrayOfMovies.map((movie) => (
+    <li key={movie.id}>
+      <a href={movie.id}>
+        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+        <p>{movie.title}</p>
+      </a>
+    </li>
+  ));
 
-const renderedArray = arrayOfMovies.map((movie) => 
-  <li key={movie.id}>
-    <a href={movie.id}>
-      <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
-      <p>{movie.title}</p>
-    </a>
-  </li>
-);
-
-return (
-  <>
-  <h1>Trendujące dzisiaj</h1>
-  <ul className="list--of--trends">
-    {renderedArray}
-  </ul>
-  </>
-);
-
+  return (
+    <>
+      <h1>Trendujące dzisiaj</h1>
+      <ul className="list--of--trends">{renderedArray}</ul>
+    </>
+  );
 }
 
 
 function RenderRandomTrendMovie(props) {
-
   const arrayOfMovies = props.arrayOfMovies;
   const randomInteger = Math.floor(Math.random() * 20);
-  const movie = arrayOfMovies[randomInteger]
-  
+  const movie = arrayOfMovies[randomInteger];
+
   return (
     <a href={movie.id}>
       <div className="trendingMovie">
         <h1>{movie.title}</h1>
-        <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}/>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        />
       </div>
     </a>
   );
-  
 }
 
-function RenderSearchResults(props) {
 
-  const arrayOfMovies = props.arrayOfMovies
-  const listOfSearchResults = arrayOfMovies.slice(0, 3).map((movie) => 
+function RenderSearchResults(props) {
+  const arrayOfMovies = props.arrayOfMovies;
+  const listOfSearchResults = arrayOfMovies.slice(0, 3).map((movie) => (
+
     <li key={movie.id}>
       <a href={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title}/>
-        <div className='container--info'>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="container--info">
           <h3>{movie.title}</h3>
-          <p>{movie.overview == "" ? "Brak opisu :(" : `${movie.overview.substring(0,240)}...`}</p>
+          <p>
+            {movie.overview == ""
+              ? "Brak opisu :("
+              : `${movie.overview.substring(0, 240)}...`}
+          </p>
         </div>
       </a>
     </li>
-  )
+  ));
 
-
-  return(
+  return (
     <ul className="list--of--results">
       {listOfSearchResults}
       <a>Pokaż wiecej wyników</a>
     </ul>
-  )
-
+  );
 }
 
 
+function searchMovieOrTvShow() {
+  var movieURL = `https://api.themoviedb.org/3/search/movie?api_key=87bf596c371c406133fdf1b253db9a36&language=pl&page=1&include_adult=false&query=${
+    document.getElementById("searchBar").value
+  }`;
 
-const arrayOfMovies = getResults('https://api.themoviedb.org/3/trending/all/day?api_key=87bf596c371c406133fdf1b253db9a36&language=pl')
+  var tvShowURL = `https://api.themoviedb.org/3/search/tv?api_key=87bf596c371c406133fdf1b253db9a36&language=pl&page=1&include_adult=false&query=${
+    document.getElementById("searchBar").value
+  }`;
 
-const randomMovie = ReactDOM.createRoot(document.getElementById("randomMovie"))
-randomMovie.render(<RenderRandomTrendMovie arrayOfMovies={arrayOfMovies}/>)
+  const arrayOfMovies = getResults(movieURL);
+  const arrayOfTvShows = getResults(tvShowURL);
+  const moviesAndTVshows = arrayOfTvShows.concat(arrayOfMovies);
+  const sortedList = moviesAndTVshows.sort(compareList).reverse();
 
-const trendingMovies = ReactDOM.createRoot(document.getElementById('trendingMovies'))
-trendingMovies.render(<RenderListOfTrends arrayOfMovies={arrayOfMovies}/>)
+  function compareList(a, b) {
+    return a.popularity - b.popularity;
+  }
+
+  searchArea.render(<RenderSearchResults arrayOfMovies={sortedList} />);
+}
 
 
-const searchArea = ReactDOM.createRoot(document.getElementById("searchArea"))
-document.getElementById('searchBar').addEventListener('input', () => {
+const searchArea = ReactDOM.createRoot(document.getElementById("searchArea"));
+var timer;
+const searchBar = document.getElementById("searchBar");
+searchBar.addEventListener("input", () => {
+  clearTimeout(timer);
+  timer = setTimeout(searchMovieOrTvShow, 1000);
+});
 
-  var url = (`https://api.themoviedb.org/3/search/movie?api_key=87bf596c371c406133fdf1b253db9a36&language=pl&page=1&include_adult=false&query=${document.getElementById('searchBar').value}`)
-  const listOfResults = getResults(url)
 
-  searchArea.render(<RenderSearchResults arrayOfMovies={listOfResults}/>)
 
-})
+document.getElementById("mainContainer").addEventListener("click", () => {
+  document.getElementById("searchArea").style.display = "none";
+});
 
-document.getElementById('mainContainer').addEventListener('click', () => {
-  document.getElementById('searchArea').style.display = 'none'
-})
+document.getElementById("searchBar").addEventListener("click", () => {
+  document.getElementById("searchArea").style.display = "initial";
+});
 
-document.getElementById('searchBar').addEventListener('click', () => {
-  document.getElementById('searchArea').style.display = 'initial'
-})
+
+const arrayOfMovies = getResults("https://api.themoviedb.org/3/trending/all/day?api_key=87bf596c371c406133fdf1b253db9a36&language=pl");
+
+const randomMovie = ReactDOM.createRoot(document.getElementById("randomMovie"));
+randomMovie.render(<RenderRandomTrendMovie arrayOfMovies={arrayOfMovies} />);
+
+const trendingMovies = ReactDOM.createRoot(document.getElementById("trendingMovies"));
+trendingMovies.render(<RenderListOfTrends arrayOfMovies={arrayOfMovies} />);
 
 
